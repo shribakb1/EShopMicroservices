79ac9a2cb32952407f57ccef680ac0b5c73e51ff
Shribak Bohdan <shribakb@gmail.com>
2025-09-20 20:50:04 +0300

    feat: add BuildingBlocks class library for CQRS pattern

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommand.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildingBlocks.CQRS
+{
+    // Це “порожній” Command, який не повертає результат (Unit)(аналог void)
+    public interface ICommand : ICommand<Unit>
+    {
+    }
+
+    // Це базовий Command, який повертає результат TResponse
+    public interface ICommand<out TResponse> : IRequest<TResponse>
+    {
+    }
+}
